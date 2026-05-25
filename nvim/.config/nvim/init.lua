958a8252ae6ce202075184cdb5ead1a48b4ac349
Jonas Broel <jonas.broel@posteo.de>
2026-05-25 19:14:57 +0200

    update neovim options

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,20 +1,34 @@
--------------
--- Options --
--------------
+---- Options ----
 
--- line numbers
 vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.wrap = false
+vim.opt.smartindent = true
 
--- tab stuff
 local tabwidth = 4
 vim.opt.tabstop = tabwidth
 vim.opt.softtabstop = tabwidth
 vim.opt.shiftwidth = tabwidth
 vim.opt.expandtab = true
 
--- search
 vim.opt.smartcase = true
 vim.opt.ignorecase = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = vim.fn.stdpath("data") .. "/undodir"
+vim.opt.undofile = true
+
+vim.opt.colorcolumn = "80"
+vim.opt.signcolumn = "yes"
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking (copying) text",
+    callback = function()
+        vim.hl.on_yank()
+    end,
+})
