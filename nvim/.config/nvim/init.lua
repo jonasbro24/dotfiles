c12c0f9d0bea57a66a0c35ff8817866bdd18549e
Jonas Broel <jonas.broel@posteo.de>
2026-05-30 22:12:58 +0200

    add treesitter to neovim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -47,3 +47,44 @@ vim.keymap.set("v", ">", ">gv", { desc = "Indent and keep selection" })
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "move down in buffer with cursor centered" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "move up in buffer with cursor centered" })
+
+---- Packages ----
+
+vim.pack.add({
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter", branch = "main" },
+})
+
+
+---- Treesitter ----
+
+local treesitter = require("nvim-treesitter")
+
+local ensure_installed = {
+    "lua",
+    "python",
+    "javascript",
+    "html",
+    "css",
+}
+
+treesitter.install(ensure_installed)
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "*",
+	callback = function(args)
+		local buf = args.buf
+		local ft = vim.bo[buf].filetype
+
+		local lang = vim.treesitter.language.get_lang(ft)
+		if not lang then
+			return
+		end
+
+		local ok_add = pcall(vim.treesitter.language.add, lang)
+		if not ok_add then
+			return
+		end
+
+		pcall(vim.treesitter.start, buf, lang)
+	end,
+})
